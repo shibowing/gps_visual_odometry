0c5162f2b42366c0f543e7092c68ebd75c5b7099
shibowing <shibowing@gmail.com>
2020-04-26 18:13:36 -0400

    v1

---
+++ readme.m
@@ -0,0 +1,7 @@
+
+### This package is only a part of VINS-fUSION, which can help beginner to fuse gps and visual odometry. 
+
+
+roslaunch global_fusion global_fusion.launch
+
+rosbag play 2020-04-21-00-24-44.bag
